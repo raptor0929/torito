bbbfd0a330acd4c6ba4759fcd3f3df7c7835a089
raptor0929 <flaura.dev@gmail.com>
2025-08-30 14:04:18 -0400

    update deploy commands

--- contracts/oracle.sh
+++ contracts/oracle.sh
@@ -2,8 +2,11 @@
 source .env
 
 forge script script/DeployPriceOracle.s.sol:DeployPriceOracle \
---rpc-url https://lisk.drpc.org \
---broadcast \
---verify \
--vvvv
+ --etherscan-api-key $ETHERSCAN_API_KEY \
+ --rpc-url https://lisk.drpc.org \
+ --broadcast \
+ --verifier blockscout \
+ --verifier-url 'https://blockscout.lisk.com/api/' \
+ --verify \
+ -vvvv
 
--- contracts/torito.sh
+++ contracts/torito.sh
@@ -2,8 +2,11 @@
 source .env
 
 forge script script/Torito.s.sol:ToritoScript \
---rpc-url https://lisk.drpc.org \
---broadcast \
---verify \
--vvvv \
---sig "run(address)" $ORACLE_CONTRACT_ADDRESS
+ --etherscan-api-key $ETHERSCAN_API_KEY \
+ --rpc-url https://lisk.drpc.org \
+ --broadcast \
+ --verifier blockscout \
+ --verifier-url 'https://blockscout.lisk.com/api/' \
+ --verify \
+ -vvvv \
+ --sig "run(address)" $ORACLE_CONTRACT_ADDRESS
